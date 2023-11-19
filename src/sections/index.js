14c53d5af23894b4296bf7fe10310cc725e2ec38
iamlydial <lydia.lavecchia@gmail.com>
2023-11-19 15:08:51 +0100

    built folder structure for the app

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+
+export {
+  CustomerReviews,
+  Hero,
+  Footer,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffers,
+  Subscribe,
+};
