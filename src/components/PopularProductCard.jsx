6a528417f91919f49914f32b18b0be6e76e47496
iamlydial <lydia.lavecchia@gmail.com>
2023-11-19 18:34:08 +0100

    fixed populat product card component and section style issues

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -2,10 +2,10 @@ import { star } from "../assets/icons";
 
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
-    <div classname="flex flex-1 flex-col w-full max-sm:w-full">
-      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[282px] h-[282px]" />
       <div className="mt-8 flex justify-start gap-2.5">
-        <img src={star} alt="rating" width={24} height={24} className="" />
+        <img src={star} alt="rating icon" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">
           (4.5)
         </p>
